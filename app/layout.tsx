675b66faf4d07a4db66a5d84a715b4aee11f5303
yusei53 <yusei530922@gmail.com>
2024-03-02 17:33:47 +0900

    chakra使ってみる

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { ChakraProvider } from "@chakra-ui/provider";
+// import { ChakraProvider } from "@chakra-ui/react";でも良いが、bundleqizeが大きくなるので、@chakra-ui/providerを使って軽量化
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body style={{ margin: 0 }}>
+        <ChakraProvider>{children}</ChakraProvider>
+      </body>
     </html>
   );
 }

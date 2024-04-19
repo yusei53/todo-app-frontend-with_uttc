6b941bd11f1e888a24f1ee751ab6bf59e9266f16
yusei53 <yusei530922@gmail.com>
2024-04-19 18:05:22 +0900

    containerのデフォルトをxlに変更

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -2,7 +2,7 @@ import { Container } from "@chakra-ui/react";
 
 const Main = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Container as="main" maxW="container.lg">
+    <Container as="main" maxW="container.xl">
       {children}
     </Container>
   );

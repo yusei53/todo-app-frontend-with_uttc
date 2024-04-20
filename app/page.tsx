3630ae4d123e30382b8262cb25fcc2d162689fbd
yusei53 <yusei530922@gmail.com>
2024-04-20 20:23:59 +0900

    Boardコンポーネントで呼び出す

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,7 @@
-"use client";
-import useSWR from "swr";
-import NotFound from "./not-found";
-import Loading from "./loading";
-import { Box } from "@chakra-ui/react";
+import Boards from "./components/Boards";
 
-type User = {
-  id: number;
-  title: string;
-  deleted: boolean;
+const MainPage = () => {
+  return <Boards />;
 };
 
-async function fetcher(key: string) {
-  return fetch(key).then((res) => res.json() as Promise<User[] | null>);
-}
-
-const Home = () => {
-  const { data, error } = useSWR("http://localhost:8083/boards", fetcher);
-
-  if (error) return <NotFound />;
-  if (!data) return <Loading />;
-  return (
-    <div>
-      {data.map((user) => (
-        <Box key={user.id}>{user.title}</Box>
-      ))}
-    </div>
-  );
-};
-
-export default Home;
+export default MainPage;

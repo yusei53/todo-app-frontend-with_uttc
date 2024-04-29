aeb3f5ea4f3fa1bd6345ce5e273862cf70d7511f
yusei53 <yusei530922@gmail.com>
2024-04-29 11:50:11 +0900

    BoardCardでページ遷移できるようにした

--- app/components/BoardCard.tsx
+++ app/components/BoardCard.tsx
@@ -1,6 +1,7 @@
 import { Box, Text } from "@chakra-ui/react";
 import { DeleteIcon } from "@chakra-ui/icons";
 import { BoardProps } from "../types/type";
+import Link from "next/link";
 
 type BoardTitle = Pick<BoardProps, "title">;
 
@@ -10,48 +11,26 @@ type TProps = {
 
 const BoardCard: React.FC<TProps> = ({ props }) => {
   return (
-    <Box
-      bg={"#EBECF0"}
-      borderRadius={3}
-      m={5}
-      position={"relative"}
-      _hover={{ bg: "#ECF5FF" }}
-    >
-      <Box display={"flex"} mx={2} py={1.5}>
-        <Text>{props.title}</Text>
-        <Box display={"flex"} alignItems={"center"}>
-          <DeleteIcon
-            sx={{ cursor: "pointer", position: "absolute", right: 4 }}
-          />
+    <Link href={`/board/${props.title}`}>
+      <Box
+        bg={"#EBECF0"}
+        borderRadius={3}
+        m={5}
+        position={"relative"}
+        transition={"0.3s"}
+        _hover={{ bg: "#6AA7E3" }}
+      >
+        <Box display={"flex"} mx={2} py={1.5}>
+          <Text>{props.title}</Text>
+          <Box display={"flex"} alignItems={"center"}>
+            <DeleteIcon
+              sx={{ cursor: "pointer", position: "absolute", right: 4 }}
+            />
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </Link>
   );
 };
 
 export default BoardCard;
-
-// import { Box, Text } from "@chakra-ui/react";
-// import { DeleteIcon } from "@chakra-ui/icons";
-// import { BoardProps } from "../types/type";
-
-// type BoardTitle = Pick<BoardProps, "title">;
-
-// type TProps = {
-//   props: BoardTitle;
-// };
-
-// const BoardCard: React.FC<TProps> = ({ props }) => {
-//   return (
-//     <Box bgColor={"#EBECF0"} borderRadius={3} mt={20} mx={5}>
-//       <Box display={"flex"} my={1} mx={4}>
-//         <Text pr={250}>{props.title}</Text>
-//         <Box display={"flex"} alignItems={"center"}>
-//           <DeleteIcon sx={{ cursor: "pointer" }} />
-//         </Box>
-//       </Box>
-//     </Box>
-//   );
-// };
-
-// export default BoardCard;

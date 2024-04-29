c719e34e00515857750c9bcee803f44c824ab1b8
yusei53 <yusei530922@gmail.com>
2024-04-29 11:46:12 +0900

    CategoriesAreaを作成

---
+++ app/components/CategoriesArea.tsx
@@ -0,0 +1,35 @@
+"use client";
+import NotFound from "../not-found";
+import Loading from "../loading";
+import { Box } from "@chakra-ui/react";
+import CategoryCard from "./CategoryCard";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+type BoardProps = {
+  id: number;
+  title: string;
+  deleted: boolean;
+};
+
+const CategoriesArea = () => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["boards"],
+    queryFn: async () => {
+      const { data } = await axios.get("http://localhost:8083/boards");
+      return data;
+    },
+  });
+
+  if (error) return <NotFound />;
+  if (isLoading) return <Loading />;
+  return (
+    <Box display="flex" overflow={"auto"} whiteSpace={"pre"} mt={10}>
+      {data.map((boardData: BoardProps) => (
+        <CategoryCard key={boardData.id} props={boardData} />
+      ))}
+    </Box>
+  );
+};
+
+export default CategoriesArea;

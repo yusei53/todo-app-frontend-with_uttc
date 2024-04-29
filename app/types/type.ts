688191779e6f52a8a003f2207413faa0e4715be4
yusei53 <yusei530922@gmail.com>
2024-04-29 11:45:32 +0900

    CategoryPropsを定義

--- app/types/type.ts
+++ app/types/type.ts
@@ -1,5 +1,11 @@
 export type BoardProps = {
   id: number;
+  title: string;
+  deleted: boolean;
+};
+
+export type CategoryProps = {
+  id: number;
   boardId: number;
   title: string;
   deleted: boolean;

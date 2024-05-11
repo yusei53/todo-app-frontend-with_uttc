16f9a50d30442cddcac61cec57a88a6a19fa32fb
yusei53 <yusei530922@gmail.com>
2024-05-11 14:15:34 +0900

    useQueryを使ってデータフェッチする

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,14 +1,34 @@
 "use client";
 import { Box, Text } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { useSearchParams } from "next/navigation";
+import { CategoryProps } from "../types/type";
 
 const BoardPage = () => {
   const searchParams = useSearchParams();
   const categoryName = searchParams.get("id");
 
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["categories", categoryName],
+    queryFn: () =>
+      axios
+        .get(`http://localhost:8083/categories?board_id=${categoryName}`)
+        .then((res) => res.data),
+  });
+  console.log(data);
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
   return (
     <Box display={"flex"}>
-      <Text>{categoryName}</Text>
+      {data.map((category: CategoryProps) => (
+        <Box key={category.id} p={5}>
+          <Text fontSize="xl">{category.title}</Text>
+        </Box>
+      ))}
     </Box>
   );
 };

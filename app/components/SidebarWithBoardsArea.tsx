3c235cb3bd81f82a7beeaebcd878f995bdbf42ee
yusei53 <yusei530922@gmail.com>
2024-05-21 00:43:03 +0900

    postリクエストを実装

--- app/components/SidebarWithBoardsArea.tsx
+++ app/components/SidebarWithBoardsArea.tsx
@@ -1,10 +1,15 @@
 "use client";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, useDisclosure } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 import useSWR from "swr";
 import Loading from "../loading";
 import NotFound from "../not-found";
 import BoardCard from "./BoardCard";
 import { BoardProps } from "../types/type";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import CreateBoardModal from "./CreateBoardModal";
 
 async function fetcher(key: string) {
   return fetch(key).then((res) => res.json() as Promise<BoardProps[] | null>);
@@ -12,6 +17,7 @@ async function fetcher(key: string) {
 
 const SideBarWithBoardsArea = () => {
   const { data, error } = useSWR("http://localhost:8083/boards", fetcher);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   // ReactQueryでの取得
   // const CategoriesArea = () => {
@@ -22,6 +28,18 @@ const SideBarWithBoardsArea = () => {
   //     return data;
   //   },
   // });
+  const [newBoardName, setNewBoardName] = useState("");
+
+  const mutation = useMutation({
+    mutationFn: (newBoard: { newBoard: string }) => {
+      return axios.post("http://localhost:8083/boards", newBoard);
+    },
+  });
+
+  const handleSave = () => {
+    mutation.mutate({ newBoard: newBoardName });
+    onClose();
+  };
 
   if (error) return <NotFound />;
   if (!data) return <Loading />;
@@ -33,11 +51,29 @@ const SideBarWithBoardsArea = () => {
       p={5}
       boxShadow={"2px 2px 4px"}
     >
-      <Box display={"flex"} alignItems={"center"} mb={5}>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        mb={5}
+      >
         <Heading fontSize={17} color={"white"}>
           ボード一覧
         </Heading>
+        <AddIcon
+          boxSize={3}
+          color={"white"}
+          onClick={onOpen}
+          _hover={{ transform: "scale(1.3)" }}
+          sx={{ cursor: "pointer", mx: 1, transition: "0.3s" }}
+        />
       </Box>
+      <CreateBoardModal
+        isOpen={isOpen}
+        onClose={onClose}
+        onChange={(e) => setNewBoardName(e.target.value)}
+        onSave={handleSave}
+      />
       {data.map((boardData) => (
         <BoardCard key={boardData.id} props={boardData} />
       ))}

75918f0581fd89673c048dabf8e61b6f78e4ec45
yusei53 <yusei530922@gmail.com>
2024-06-06 22:54:25 +0900

    createBoardのリファクタと、deleteBoardの作成

--- app/api/boards/queryFn.ts
+++ app/api/boards/queryFn.ts
@@ -5,10 +5,16 @@ export const fetchBoards = async () => {
   return data;
 };
 
-export const createBoard = async (board_title: { board_title: string }) => {
-  const { data } = await axios.post(
-    "http://localhost:8083/boards",
-    board_title
+export const createBoard = async (board_title: string) => {
+  const { data } = await axios.post("http://localhost:8083/boards", {
+    board_title,
+  });
+  return data;
+};
+
+export const deleteBoard = async (board_id: number) => {
+  const { data } = await axios.delete(
+    `http://localhost:8083/boards/${board_id}`
   );
   return data;
 };

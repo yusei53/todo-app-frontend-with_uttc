14c46421836ed58a6e5b703643cbaf8163bb6155
yusei53 <yusei530922@gmail.com>
2024-06-06 22:54:41 +0900

    BoardCardにdelete追加

--- app/components/molecules/BoardCard.tsx
+++ app/components/molecules/BoardCard.tsx
@@ -1,17 +1,21 @@
 import { Box, Text } from "@chakra-ui/react";
 import { DeleteIcon } from "@chakra-ui/icons";
-import { BoardProps } from "../../types/type";
 import Link from "next/link";
 
-type BoardTitle = Omit<BoardProps, "deleted">;
-
 type TProps = {
-  props: BoardTitle;
+  id: number;
+  title: string;
+  handleDelete: () => void;
 };
 
-const BoardCard: React.FC<TProps> = ({ props }) => {
+const BoardCard: React.FC<TProps> = ({ id, title, handleDelete }) => {
+  const handleDeleteClick = (e: React.MouseEvent<SVGElement>) => {
+    e.preventDefault();
+    handleDelete();
+  };
+
   return (
-    <Link href={`/board/?id=${props.id}`}>
+    <Link href={`/board/?id=${id}`}>
       <Box
         bg={"#EBECF0"}
         borderRadius={3}
@@ -24,11 +28,12 @@ const BoardCard: React.FC<TProps> = ({ props }) => {
         my={3}
         _hover={{ bg: "#6AA7E3" }}
       >
-        <Text>{props.title}</Text>
+        <Text>{title}</Text>
         <DeleteIcon
           boxSize={3.5}
           _hover={{ transform: "scale(1.2)" }}
           sx={{ cursor: "pointer", mx: 1, transition: "0.3s" }}
+          onClick={handleDeleteClick}
         />
       </Box>
     </Link>

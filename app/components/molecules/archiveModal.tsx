a50218f05037ca126005d6228d2a764cd930010d
yusei53 <yusei530922@gmail.com>
2024-06-08 00:29:18 +0900

    ArchiveModalの作成

---
+++ app/components/molecules/archiveModal.tsx
@@ -0,0 +1,43 @@
+import {
+  Button,
+  Text,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@chakra-ui/react";
+
+type TProps = {
+  boardTitle: string;
+  isOpen: boolean;
+  onClose: () => void;
+  onSave: () => void;
+};
+
+const ArchiveModal: React.FC<TProps> = ({
+  boardTitle,
+  isOpen,
+  onClose,
+  onSave,
+}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalContent>
+        <ModalHeader>{boardTitle}の削除</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text fontWeight="bold">本当に削除しますか？</Text>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" onClick={onSave}>
+            削除
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ArchiveModal;

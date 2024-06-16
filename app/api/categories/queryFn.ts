f160d79ccd360d5ac4f4e01e06bed6758f2cf16b
yusei53 <yusei530922@gmail.com>
2024-06-17 00:06:04 +0900

    createCategoryを作成

--- app/api/categories/queryFn.ts
+++ app/api/categories/queryFn.ts
@@ -1,8 +1,22 @@
 import axios from "axios";
 
-export const fetchCategories = async (boardId: string | null) => {
+export const fetchCategories = async (boardId: number | null) => {
   const { data } = await axios.get(
     `http://localhost:8083/categories?board_id=${boardId}`
+  );
+  return data;
+};
+
+export const createCategory = async (
+  boardId: number | null,
+  categoryTitle: string
+) => {
+  const { data } = await axios.post(
+    `http://localhost:8083/categories?board_id=${boardId}`,
+    {
+      board_id: boardId,
+      title: categoryTitle,
+    }
   );
   return data;
 };

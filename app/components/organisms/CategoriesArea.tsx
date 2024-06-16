e84b4ecd0a8926a7114c614ba242d5a774bd9630
yusei53 <yusei530922@gmail.com>
2024-06-17 00:07:47 +0900

    CategoriesAreaでpostを呼ぶ

--- app/components/organisms/CategoriesArea.tsx
+++ app/components/organisms/CategoriesArea.tsx
@@ -2,18 +2,20 @@
 import Loading from "../../loading";
 import { Box } from "@chakra-ui/react";
 import CategoryCard from "../molecules/CategoryCard";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSearchParams } from "next/navigation";
 import { CategoryProps } from "../../types/type";
 import AddButton from "../atom/AddButton";
 import { useCallback, useState } from "react";
-import TextAreaButtonGroup from "../molecules/TextAreaButtonGroup";
+import TextAreaButtonGroup from "../atom/TextAreaButtonGroup";
 import CategoryCardContainer from "../atom/CategoryCardContainer";
-import { fetchCategories } from "@/app/api/categories/queryFn";
+import { createCategory, fetchCategories } from "@/app/api/categories/queryFn";
 
 const CategoriesArea = () => {
   const searchParams = useSearchParams();
-  const boardId = searchParams.get("id");
+  const boardId = Number(searchParams.get("id"));
+  const [newCategoryTitle, setNewCategoryTitle] = useState("");
+  const queryClient = useQueryClient();
 
   const [isOepn, setIsOpen] = useState(false);
 
@@ -25,6 +27,24 @@ const CategoriesArea = () => {
   const handleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
+
+  const createMutation = useMutation({
+    mutationFn: ({
+      boardId,
+      categoryTitle,
+    }: {
+      boardId: number;
+      categoryTitle: string;
+    }) => createCategory(boardId, categoryTitle),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["categories", boardId] });
+      setIsOpen(false);
+    },
+  });
+
+  const handleSave = () => {
+    createMutation.mutate({ boardId, categoryTitle: newCategoryTitle });
+  };
 
   if (isLoading) return <Loading />;
   if (!data) return <div>dataがありません</div>;
@@ -51,7 +71,9 @@ const CategoriesArea = () => {
             <TextAreaButtonGroup
               title={"リストを追加"}
               placeholder={"リストのタイトルを入力"}
-              handleOpen={handleOpen}
+              onChange={(e) => setNewCategoryTitle(e.target.value)}
+              onSave={handleSave}
+              onClose={handleOpen}
             />
           </CategoryCardContainer>
         ) : (

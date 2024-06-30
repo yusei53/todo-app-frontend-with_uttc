b191207df2aa13da362a9a73b79a572acef56cdc
yusei53 <yusei530922@gmail.com>
2024-07-01 00:15:18 +0900

    BoardDetailsTemplateの作成

---
+++ app/components/templates/BoardDetailsTemplate.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { fetchCategories, createCategory } from "@/app/api/categories/queryFn";
+import Loading from "@/app/loading";
+import { useQueryClient, useMutation, useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { useState, useCallback } from "react";
+import CategoriesArea from "../organisms/CategoriesArea";
+
+const BoardDetailsTemplate = () => {
+  const searchParams = useSearchParams();
+  const boardId = Number(searchParams.get("id"));
+  const [newCategoryTitle, setNewCategoryTitle] = useState("");
+  const queryClient = useQueryClient();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["categories", boardId],
+    queryFn: () => fetchCategories(boardId),
+  });
+
+  const handleClose = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+  const createMutation = useMutation({
+    mutationFn: ({
+      boardId,
+      categoryTitle,
+    }: {
+      boardId: number;
+      categoryTitle: string;
+    }) => createCategory(boardId, categoryTitle),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["categories", boardId] });
+      setIsOpen(false);
+    },
+  });
+
+  const handleSave = () => {
+    createMutation.mutate({ boardId, categoryTitle: newCategoryTitle });
+  };
+
+  if (isLoading) return <Loading />;
+  if (!data) return <div>dataがありません</div>;
+
+  return (
+    <CategoriesArea
+      categoryData={data}
+      isOpen={isOpen}
+      onChange={(e) => setNewCategoryTitle(e.target.value)}
+      onSave={handleSave}
+      onClose={handleClose}
+    />
+  );
+};
+
+export default BoardDetailsTemplate;

3e4c16f49ba5861c91a7bd2dbbaf1003d92d8af3
yusei53 <yusei530922@gmail.com>
2024-07-25 11:43:33 +0900

    createItemsを作成

--- app/api/items/queryFn.ts
+++ app/api/items/queryFn.ts
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-export const fetchItems = async (categoryId: number | null) => {
+export const fetchItems = async (categoryId: number) => {
   const { data } = await axios.get(
     `http://localhost:8083/items?category_id=${categoryId}`
   );
   return data;
 };
 
-// export const createItems = async (
-//   categoryId: number | null,
-//   itemTitle: string,
-//   itemContent: string,
-//   itemExpiredAt: Date
-// ) => {
-//   const { data } = await axios.post(
-//     `http://localhost:8083/items?category_id=${categoryId}`,
-//     {
-//       category_id: categoryId,
-//       title: itemTitle,
-//       content: itemContent,
-//       expired_at: itemExpiredAt,
-//     }
-//   );
-//   return data;
-// };
+export const createItems = async (
+  categoryId: number,
+  itemTitle: string,
+  itemContent: string,
+  ExpiredAt: string
+) => {
+  const { data } = await axios.post(
+    `http://localhost:8083/items?category_id=${categoryId}`,
+    {
+      category_id: categoryId,
+      item_title: itemTitle,
+      content: itemContent,
+      expired_at: ExpiredAt,
+    }
+  );
+  return data;
+};

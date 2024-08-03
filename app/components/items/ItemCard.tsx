b1ed780ce2075acd4463688169279186baf1bc6e
yusei53 <yusei530922@gmail.com>
2024-08-03 09:45:54 +0900

    ItemCardにcontentとexpiredを追加

--- app/components/items/ItemCard.tsx
+++ app/components/items/ItemCard.tsx
@@ -1,12 +1,54 @@
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  Box,
+  Text,
+  AccordionIcon,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
-const ItemCard = ({ title }: { title: string }) => {
+type TProps = {
+  itemTitle: string;
+  itemContent: string;
+  expiredAt: string;
+};
+
+const ItemCard: React.FC<TProps> = ({ itemTitle, itemContent, expiredAt }) => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <Box bgColor={"white"} boxShadow={"0 0 2px gray"} borderRadius={5} my={2}>
-      <Text isTruncated whiteSpace={"pre-wrap"} py={1} px={3} fontSize={14}>
-        {title}
-      </Text>
-    </Box>
+    <Accordion allowMultiple>
+      <AccordionItem border={"none"}>
+        <Box
+          bgColor={"white"}
+          boxShadow={"0 0 2px gray"}
+          borderRadius={5}
+          my={2}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <AccordionButton
+            py={1.5}
+            justifyContent={"space-between"}
+            _hover={{ bgColor: "none" }}
+          >
+            <Text isTruncated fontSize={14}>
+              {itemTitle}
+            </Text>
+            {isHovered && <AccordionIcon />}
+          </AccordionButton>
+          <AccordionPanel py={0.5}>
+            <Text whiteSpace={"pre-wrap"} fontSize={13}>
+              {itemContent}
+            </Text>
+            <Text whiteSpace={"pre-wrap"} py={1} fontSize={13}>
+              期限: {expiredAt}
+            </Text>
+          </AccordionPanel>
+        </Box>
+      </AccordionItem>
+    </Accordion>
   );
 };
 
